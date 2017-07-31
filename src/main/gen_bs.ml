50716760835e512906a626ed1cf481d8bbb2b6df
0zat <0.zat.zer0@gmail.com>
2017-07-31 22:35:56 +0900

    add arg option

--- src/main/gen_bs.ml
+++ src/main/gen_bs.ml
@@ -1,9 +1,16 @@
+let print_version () = print_endline "0.0.0"
+
 let desc_dir =
   "generate bucklescript code from Web IDL files in a directory"
 
 
 let speclist = [
-  ("-dir", (Arg.String Webidl_to_bs.from_dir), desc_dir)
+  ("-dir", (Arg.String Webidl_to_bs.from_dir), desc_dir);
+  ("-version", Arg.Unit print_version, "show version") 
 ]
 
-let () = Arg.parse speclist (fun _ -> ()) "options: "
+let desc = "available options: -dir <dir>, -version"
+
+let proc_anonymous_arg arg = print_endline desc
+
+let () = Arg.parse speclist proc_anonymous_arg desc
